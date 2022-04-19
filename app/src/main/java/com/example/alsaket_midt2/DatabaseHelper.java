7b3d4b26355d129e817f587b0c34cd8ef490cf1a
asmalsaket <100064771+asmalsaket@users.noreply.github.com>
2022-04-19 13:50:00 +0300

    class to create database and manage it (add, view, delete)

---
+++ app/src/main/java/com/example/alsaket_midt2/DatabaseHelper.java
@@ -0,0 +1,58 @@
+package com.example.alsaket_midt2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.SparseArray;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "Alsaket-mid2.db";
+    private static final String TABLE_NAME = "PERSON";
+    private static final String COLUMN_ID = "ID";
+    private static final String COLUMN_NAME = "Name";
+    private static final String COLUMN_NID = "NID";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(" CREATE TABLE " + TABLE_NAME + "(" +COLUMN_ID
+                + " INTEGER PRIMARY KEY," + COLUMN_NAME + " TEXT NOT NULL, " +
+                COLUMN_NID + " INTEGER NOT NULL) ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void Add(int id, String name, int nid){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_ID, id);
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_NID, nid);
+
+        db.insert(TABLE_NAME, null, values);
+
+    }
+    public Cursor View(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor x = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return x;
+    }
+
+    public Integer Delete(String id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] {id});
+    }
+}
